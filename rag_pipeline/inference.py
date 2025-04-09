058964f65b0638be19c21484e971a31ae6a7b2ae
Amirul <amirulal73@gmail.com>
2025-04-09 14:05:27 +0800

    Completed cimb set 4

--- rag_pipeline/inference.py
+++ rag_pipeline/inference.py
@@ -75,11 +75,11 @@ def extract_information(docs:list):
     context = docs
 
     questions_and_models = [
-        ("What is the CHURN PATISSERIE SDN. BHD information?", BorrowerInformation, "transformed_files/27692-SSM CHURN PATISSERIE SDN. BHD_page_1_extracted_transformed.txt"), 
-        ("What is the bank information?", BankInformation, "transformed_files/27692-LO_page_11_extracted_transformed.txt"), 
-        ("What is the loan information?", LoanInformation, "transformed_files/27692-LO_page_4_extracted_transformed.txt"),
-        ("What is the BEYOND LEGEND GROUP SDN. BHD information?", GuarantorInformation, "transformed_files/27692-SSM BEYOND LEGEND GROUP SDN. BHD_page_1_extracted_transformed.txt"), 
-        ("What is the law firm information?", LawFirmInformation, "transformed_files/27692-LO_page_3_extracted_transformed.txt")
+        ("What is the Play Interactive Sdn. Bhd. information?", BorrowerInformation, "transformed_files/SSM dd 26-03-25_page_1_extracted_transformed.txt"), 
+        ("What is the bank information?", BankInformation, "transformed_files/LO dd 21-03-25_page_1_extracted_transformed.txt"), 
+        ("What is the loan information?", LoanInformation, "transformed_files/LO dd 21-03-25_page_1_extracted_transformed.txt"),
+        ("What is the Guarantees information?", GuarantorInformation, "transformed_files/LO dd 21-03-25_page_7_extracted_transformed.txt"), 
+        ("What is the law firm information?", LawFirmInformation, "transformed_files/LO dd 21-03-25_page_1_extracted_transformed.txt")
     ]
 
     extracted_information = {}
