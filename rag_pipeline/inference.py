cbc899516dae391a42abe83076c130deaadca31d
Amirul <amirulal73@gmail.com>
2025-04-14 16:42:56 +0800

    Completed UOB set 3

--- rag_pipeline/inference.py
+++ rag_pipeline/inference.py
@@ -15,7 +15,10 @@ from rag_pipeline.detail_information import (
                                                 BankInformation,
                                                 LoanInformation,
                                                 GuarantorInformation,
-                                                LawFirmInformation
+                                                LawFirmInformation,
+                                                TitleInformation,
+                                                PropertyInformation,
+                                                FacilityInformation,
                                             )
 
 load_dotenv()
@@ -54,7 +57,7 @@ def rag_workflow(question:str,model_class:Type[BaseModel],path:str):
     prompt_template = """
         You are an assistant for question-answering tasks. Use the following retrieved context to answer the question. 
         Only output the answer itself, without any additional explanation or commentary. 
-        If there is no relevant information or you don't know the answer, reply with an empty string ("") or null.
+        If there is no relevant information or you don't know the answer, reply with an empty string null.
         
         Question: {question}
         Context: {context}
@@ -86,11 +89,14 @@ def extract_information(docs:list):
     context = docs
 
     questions_and_models = [
-        ("What is the CHURN PATISSERIE SDN. BHD information?", BorrowerInformation, "transformed_files/27692-SSM CHURN PATISSERIE SDN. BHD_page_1_extracted_transformed.txt"), 
-        ("What is the bank information?", BankInformation, "transformed_files/27692-LO_page_11_extracted_transformed.txt"), 
-        ("What is the loan information?", LoanInformation, "transformed_files/27692-LO_page_4_extracted_transformed.txt"),
-        ("What is the BEYOND LEGEND GROUP SDN. BHD information?", GuarantorInformation, "transformed_files/27692-SSM BEYOND LEGEND GROUP SDN. BHD_page_1_extracted_transformed.txt"), 
-        ("What is the law firm information?", LawFirmInformation, "transformed_files/27692-LO_page_3_extracted_transformed.txt")
+        ("What is the REFINED CONTOUR SDN BHD  information?", BorrowerInformation, "transformed_files/LO (1)_page_1_extracted_transformed.txt"), 
+        ("What is the bank information?", BankInformation, "transformed_files/LO (1)_page_1_extracted_transformed.txt"), 
+        ("What is the loan information?", LoanInformation, "transformed_files/LO (1)_page_1_extracted_transformed.txt"),
+        ("What is the facility information?", FacilityInformation, "transformed_files/LO (1)_page_1_extracted_transformed.txtt"),
+        ("What is the property information?", PropertyInformation, "transformed_files/LO (1)_page_4_extracted_transformed.txt"),
+        ("Formulate is the title description", TitleInformation, "transformed_files/LO (1)_page_5_extracted_transformed.txt"),
+        ("What is the Guarantees information?", GuarantorInformation, "transformed_files/LO (1)_page_2_extracted_transformed.txt"), 
+        ("What is the law firm information?", LawFirmInformation, "transformed_files\LO (1)_page_2_extracted_transformed.txt"),
     ]
 
     extracted_information = {}
