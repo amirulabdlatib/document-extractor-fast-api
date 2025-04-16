ecf1019e6aa12519aa276594f7b3f92bc1688535
Amirul <amirulal73@gmail.com>
2025-04-16 15:59:54 +0800

    Setup of local vector database

--- rag_pipeline/vector-database-v1.py
+++ rag_pipeline/vector-database-v1.py
@@ -1,2 +1,9 @@
+import weaviate
+
+
+
 if __name__ == "__main__":
     print("Run from vector database python file")
+    
+    with weaviate.connect_to_local() as client:
+        print("Weaviate Ready:", client.is_ready())
