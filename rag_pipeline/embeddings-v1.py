56c7e8a55a5956e8e8614ebaec58b0ed8588422a
Amirul <amirulal73@gmail.com>
2025-04-16 16:21:18 +0800

    Setup basic embedding model

--- rag_pipeline/embeddings-v1.py
+++ rag_pipeline/embeddings-v1.py
@@ -1,2 +1,19 @@
+import os
+from dotenv import load_dotenv
+from langchain_ollama import OllamaEmbeddings
+
+load_dotenv()
+
+
+def getEmbeddings():
+    embeddings = OllamaEmbeddings(
+        model=os.environ["EMBEDDINGS_MODEL"]
+    )
+    
+    print(embeddings)
+
+
 if __name__ == "__main__":
     print("Run from vector embedding python file")
+    
+    print(getEmbeddings())
